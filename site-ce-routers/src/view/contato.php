4787c05c18e5fb74a3210f8aedded29684e4cc9f
carlos <carlos@carlosw.(none)>
2014-11-29 20:36:35 -0200

    commit projeto fase 2 ajustando rotas

---
+++ site-ce-routers/src/view/contato.php
@@ -0,0 +1,66 @@
+<script type="text/javascript">
+    function Alerta() {
+        alert("Dados enviados com suceso!");
+    }
+</script>
+
+<?php 
+    if(isset($_POST["enviar"])):
+        header("Location:http://localhost:8091/contato");
+    endif;
+?>
+
+
+<?php include 'header.php' ?>
+<header class="jumbotron subhead">
+    <div class="container">
+        <h1> Contato </h1>
+    </div>
+</header>
+<form class="form-horizontal" action="#" method="post">
+    <fieldset>
+        <!-- Form Name -->
+        <legend> Contato </legend>
+
+        <!-- Text input-->
+        <div class="control-group">
+            <label class="control-label">Nome</label>
+            <div class="controls">
+                <input id="nome" name="nome" type="text" placeholder="Insira seu nome" class="input-xlarge" required>
+                <p class="help-block">* Campo Obrigatório</p>
+            </div>
+            <br>
+            <div class="contols-group">  
+                <label class="control-label">Email</label>
+                <div class="controls">
+                    <input id="email" name="email" type="email" placeholder="Insira seu email" class="input-xlarge" required>
+                    <p class="help-block">* Campo Obrigatório</p>
+                </div>
+            </div>
+            <br>
+            <div class="contols-group">  
+                <label class="control-label"> Assunto </label>
+                <div class="controls">
+                    <input id="assunto" name="assunto" type="text"  class="input-xlarge" required>
+                </div>
+            </div>
+            <br>
+            <div class="contols-group">  
+                <label class="control-label"> Mensagem </label>
+                <div class="controls">
+                    <textarea class="form-control"  name="mensagem" id="" cols="40" rows="10"></textarea>
+                </div>
+            </div>
+        </div>
+
+        <!-- Button -->
+        <div class="control-group">
+            <label class="control-label"></label>
+            <div class="controls">
+                <button id="enviar" name="enviar" class="btn btn-primary" onclick="Alerta();">Enviar</button>
+            </div>
+        </div>
+
+    </fieldset>
+</form>
+<?php include 'footer.php' ?>
---
+++ site-ce-routers/src/view/produto.php
@@ -0,0 +1,18 @@
+<head>
+    <title> Produtos </title>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+
+    <header class="jumbotron subhead">
+        <div class="container">
+            <h1> Code Education PHP </h1>
+        </div>
+    </header>	
+    <div class="row-fluid">
+        <img src="img/Em_Construcao.jpg.png" alt="#"/>
+    </div>
+    <?php include 'footer.php' ?>
+   
